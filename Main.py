45e9132d0dbb2dd8d73ed479e380cf177b8630b1
URK21CS2035-RUDALPH <109648432+URK21CS2035-RUDALPH@users.noreply.github.com>
2022-08-03 11:18:45 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,37 +1,40 @@
 import os
 class Stack:
-    def __init__(self, size):
-        self.items = [None]*size
-        self.size = size
-        self.top = -1
-    def is_empty(self):
-        # Write code here
-        return self.top == -1
-    def is_full(self):
-        # Write code here
-        return self.top == (self.size-1)
+    def __init__(self, size):
+        self.item = []
+        self.size = size
+        
+
+    def is_empty(self):
+        # Write code here
+        if len(self.item)==0 :
+            return True
+
+    def is_full(self):
+        # Write code here
+        if len(self.item)==self.size :
+            return True
+
     def push(self, data):
-        if not self.is_full():
-            # Write code here
-            self.top += 1
-            self.items[self.top] = data
-    def pop(self):
-        if not self.is_empty():
-            # Write code here
-            x = self.items[self.top]
-            self.top -= 1
-    def status(self):
-        # Write code here
-        for i in range(self.top+1):
-            print(self.items[i])
-           
+        if not self.is_full():
+            self.item.append(data)
+
+    def pop(self):
+        if not self.is_empty():
+            self.item.pop()
+
+    def status(self):
+        if not self.is_empty():
+            for i in self.item:
+                print(i)
+
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
 stack = Stack(size)
 for line in range(queries):
-    values = list(map(int, input().rstrip().split()))
-    if values[0] == 1:
-        stack.push(values[1])
-    elif values[0] == 2:
-        stack.pop()
+    values = list(map(int, input().rstrip().split()))
+    if values[0] == 1:
+        stack.push(values[1])
+    elif values[0] == 2:
+        stack.pop()
 stack.status()
