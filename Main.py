d6f5fec96489a0396345ca7a6decd279d997f589
URK21CS2035-RUDALPH <109648432+URK21CS2035-RUDALPH@users.noreply.github.com>
2022-08-03 09:25:29 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,38 +1,37 @@
-class Stack: 
-    def _init_(self, size): 
-        self.items = [None]*size 
-        self.size = size 
-        self.top = -1 
-    def is_empty(self): 
-        if self.top == -1 : 
-            return 1 
-        else : 
-            return 0 
-    def is_full(self): 
-        if self.top == (self.size - 1) : 
-            return 1 
-        else :
-            return 0 
-    def push(self, data): 
-        if not self.is_full(): 
-            self.top+=1 
-            self.items[self.top]= data 
-    def pop(self): 
-        if not self.is_empty(): 
-            del self.items[self.top] 
-            self.top-=1 
-    def status(self): 
-        if self.is_empty()!=1: 
-            for i in range(0,self.top+1): 
-                print(self.items[i])
-              
+import os
+class Stack:
+    def __init__(self, size):
+        self.items = [None]*size
+        self.size = size
+        self.top = -1
+    def is_empty(self):
+        # Write code here
+        return self.top == -1
+    def is_full(self):
+        # Write code here
+        return self.top == (self.size-1)
+    def push(self, data):
+        if not self.is_full():
+            # Write code here
+            self.top += 1
+            self.items[self.top] = data
+    def pop(self):
+        if not self.is_empty():
+            # Write code here
+            x = self.items[self.top]
+            self.top -= 1
+    def status(self):
+        # Write code here
+        for i in range(self.top+1):
+            print(self.items[i])
+           
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
 stack = Stack(size)
-for line in range(queries): 
-    values = list(map(int, input().rstrip().split())) 
-    if values[0] == 1: 
-        stack.push(values[1]) 
-    elif values[0] == 2: 
-        stack.pop()
+for line in range(queries):
+    values = list(map(int, input().rstrip().split()))
+    if values[0] == 1:
+        stack.push(values[1])
+    elif values[0] == 2:
+        stack.pop()
 stack.status()
